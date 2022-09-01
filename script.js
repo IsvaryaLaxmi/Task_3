995b19c304b8f1148dabac5c940cd154e37b3014
IsvaryaLaxmi <103510875+IsvaryaLaxmi@users.noreply.github.com>
2022-09-01 12:41:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,33 +26,34 @@ inputs.forEach((input)=>{
     });
 });
 
-var list1 =[];
-var list2 =[];
-var list3 =[];
-var list4 =[];
+// var list1 =[];
+// var list2 =[];
+// var list3 =[];
+// var list4 =[];
 
 var n = 1;
-var x = 0;
+// var x = 0;
 
 function sub1()
 {
  var AddRown= document.getElementById('show');
  var NewRow= AddRown.insertRow(n);
+var list={
 
- list1[x]= document.getElementById("Firstname").value;
- list2[x]= document.getElementById("Lastname").value;
- list3[x]= document.getElementById("Email").value;
- list4[x]= document.getElementById("telephone").value;
-
+Firstname : document.getElementById("Firstname").value,
+Lastname : document.getElementById("Lastname").value,
+Email : document.getElementById("Email").value,
+telephone : document.getElementById("telephone").value
+}
  var cel1 = NewRow.insertCell(0);
  var cel2 = NewRow.insertCell(1);
-var cel3 = NewRow.insertCell(2);
+ var cel3 = NewRow.insertCell(2);
  var cel4 = NewRow.insertCell(3);
 
- cel1.innerHTML = list1[x];
- cel2.innerHTML = list2[x];
- cel3.innerHTML = list3[x];
- cel4.innerHTML = list4[x];
+ cel1.innerHTML = list.Firstname;
+ cel2.innerHTML = list.Lastname;
+ cel3.innerHTML = list.Email;
+ cel4.innerHTML = list.telephone;
 
  n++;
  
@@ -63,6 +64,4 @@ let inputs=document.querySelectorAll('#Firstname,#Lastname,#Email,#telephone');
      inputs.forEach(input => input.value  = '' )
 
  });
-
-
 }
